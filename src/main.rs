7ddf19bd3403bc88bf056a95700f03f4eca53b8b
Mateusz Matejuk (esavier) <esavier@erglabs.org>
2021-10-10 14:52:43 +0200

    event loop WIP

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,252 @@
+#![allow(unused_imports)]
+
 use bytes::{ Bytes, BytesMut};
-use std::net::SocketAddr;
+use mio::unix::pipe::new;
+use snow::types::Hash;
+use time::error;
+use std::{net::SocketAddr, str::from_utf8};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_stream::StreamExt;
 use tokio_util::codec::Framed;
+use tokio::sync::{
+  broadcast,
+  mpsc,
+};
 use futures::SinkExt;
-use anyhow::Result;
+use anyhow::{Result, bail, anyhow};
 use clap::App;
 use tracing;
 pub mod tokio_netstring;
-use tokio_netstring::NetstringCodec;
+use tokio_netstring::*;
+use std::collections::HashMap;
+use tokio::time::{sleep, Duration};
+struct CommChannel {
+  sender: tokio::sync::broadcast::Sender<UplinkEvent>
+}
+
+impl CommChannel {
+  pub fn new() -> Self {
+    let (sender, _): (tokio::sync::broadcast::Sender<UplinkEvent>,_)  = tokio::sync::broadcast::channel(64);
+    CommChannel {
+      sender,
+    }
+  }
+}
+#[derive(Clone, Debug)]
+enum UplinkEvent {
+  MessageReceived(MessageType),
+  MessageProduced(MessageType),
+  Heartbeat,
+  ConnectionBroken,
+  Void,
+}
+
+struct NetUser {
+  transport: Framed<TcpStream, NetstringCodec>,
+}
+
+impl NetUser {
+  async fn await_message(&mut self) -> anyhow::Result<MessageType> {
+    let minwait : usize = 10;
+    let maxwait : usize = 250;
+    let mut waittime : usize = minwait; //msec
+    loop {
+      let opt = self.transport.next().await;
+      match opt {
+        Some(res) => {
+          waittime = minwait;
+          tracing::info!("got something");
+          match res {
+            Ok(msg) => {
+              tracing::info!("got proper message");
+              return Ok(msg)
+            }
+            Err(e) => {
+              tracing::info!("got error{}", e);
+              bail!(e)
+            }
+          }
+        }
+        None => {
+          if waittime < maxwait { waittime = waittime+10 }
+          tracing::info!("got nothing, wait {}", waittime);
+          tokio::time::sleep(Duration::from_millis(waittime as u64)).await;
+        }
+      }
+    }
+    // while let Some(message) =  self.transport.next().await {
+    //   return message.map_err(|e| anyhow::Error::new(e) )
+    // };
+    // bail! ("connection broken?");
+  }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let matches = App::new("simple").args_from_usage("-s --server 'Server mode'").get_matches();
-    let addr = "127.0.0.1:20000".to_string();
-
-    if matches.is_present("server") {
-        let socket = TcpListener::bind(&addr).await?;
-        loop {
-            let (connection, clientaddr) = socket.accept().await?; 
-            tokio::spawn(async move {
-                handle_connection(connection, clientaddr).await;
-            });
-        }
-    } else {
-        let connection = TcpStream::connect(&addr).await?;
-        let mut transport = tokio_util::codec::Framed::new(connection, tokio_netstring::NetstringCodec);
-        transport.send(bytes::Bytes::from("Hello World")).await?;
-    }
-
-    Ok(())
-}
-
-async fn server_process(transport: &mut Framed<TcpStream, NetstringCodec>, m: &Bytes) -> Result<()> {
-    let mut buf = BytesMut::with_capacity(m.len() + 20);
-    buf.extend_from_slice(&m);
-    buf.extend_from_slice(b" <- got this, i am echoing!");
-    transport.send(buf.freeze()).await?;
-    Ok(())
-}
-
-async fn handle_connection(connection: TcpStream, connection_address: SocketAddr) {
-    tracing::info!("conenction from {}, handling", connection_address);
-    let mut transport = tokio_util::codec::Framed::new(connection, tokio_netstring::NetstringCodec);
+  let matches = App::new("simple").args_from_usage("-s --server 'Server mode'").get_matches();
+  let addr = "127.0.0.1:20000".to_string();
+  tracing_subscriber::fmt::init();
+  tracing::info!("started");
+  if matches.is_present("server") {
+    let socket = TcpListener::bind(&addr).await?;
+    let mut commchannel = CommChannel::new();
     loop {
-        while let Some(res) = transport.next().await {
-            match res {
-                Err(e) => tracing::error!("got error {}", e),
-                Ok(m) => {
-                    match server_process(&mut transport, &m.freeze()).await {
-                        Ok(()) => (),
-                        Err(e) => tracing::error!("error encountered {}", e),
-                    }
-                }
-            }
-        };
+      tracing::info!("new connection");
+      let (connection, clientaddr) = socket.accept().await?;
+      let channel = commchannel.sender.clone();
+      tokio::spawn(async move {
+        handle_connection(connection, clientaddr,  channel ).await;
+      });
     }
+  } else {
+    let connection = TcpStream::connect(&addr).await?;
+    let mut transport = tokio_util::codec::Framed::new(connection, tokio_netstring::NetstringCodec);
+    // transport.send(MessageType::Message { payload : { BytesMut::new().extend_from_slice(b"Hello World"), }).await?;
+    transport.send(newStandardMessage_string("HelloWorld".to_owned()).await).await;
+    let out = transport.next().await;
+    tracing::info!("got {:?}", out);
+  }
+
+  Ok(())
+}
+
+// async fn server_process(transport: &mut Framed<TcpStream, NetstringCodec>, m: &Bytes) -> Result<()> {
+//   let mut buf = BytesMut::with_capacity(m.len() + 20);
+//   buf.extend_from_slice(&m);
+//   buf.extend_from_slice(b" <- got this, i am echoing!");
+//   transport.send(buf.freeze()).await?;
+//   Ok(())
+// }
+
+async fn heartbeat(milis: Option<u64>) -> UplinkEvent {
+  if milis.is_none() {
+    tokio::time::sleep(Duration::from_millis(1000)).await;
+  } else {
+    let mut time = milis.unwrap();
+    if time < 1000 { time = 1000 };
+    tokio::time::sleep(Duration::from_millis(time)).await;
+  };
+  UplinkEvent::Heartbeat
+}
+
+async fn handle_connection_io(mut netuser : NetUser, mut io_input: tokio::sync::broadcast::Receiver<UplinkEvent>, mut io_output: tokio::sync::broadcast::Sender<UplinkEvent>) -> anyhow::Result<()> {
+  loop {
+    let out = tokio::select! {
+      message = netuser.await_message() => {
+        if let Ok(data) = message {
+          UplinkEvent::MessageReceived(data)
+        } else { 
+          UplinkEvent::ConnectionBroken 
+        }
+      }
+      event = heartbeat(None) => {
+        event
+      }
+      tosend = io_input.recv() => {
+        match tosend {
+          Ok(x) => x,
+          Err(e) => {
+            UplinkEvent::Void
+          } 
+        }
+      }
+    };
+    match out {
+      UplinkEvent::Heartbeat => {
+        netuser.transport.send(MessageType::Heartbeat).await?
+      }
+      UplinkEvent::ConnectionBroken  => {
+        tracing::info!("breaking connection");
+        break
+      }
+      UplinkEvent::MessageProduced(m) => {
+        tracing::info!("sending shit");
+        netuser.transport.send(m).await? // write motherfucker as is
+      }
+      UplinkEvent::MessageReceived(m) => {
+        tracing::info!("sending shit");
+        netuser.transport.send(m).await? // just echo for now //todo
+        // todo io_output.send() // this shit into orbit
+      }
+      _ => {
+        tracing::info!("shit happened at io controller");
+      }
+    }
+  }
+  Ok(())
+}
+
+
+// get stream,
+// create transport
+// spawn io driver for user
+// join default (only) channel which is basically a broadcast
+// they saw me rolling, thay hated
+async fn handle_connection(connection: TcpStream, connection_address: SocketAddr, commchannel : tokio::sync::broadcast::Sender<UplinkEvent>) {
+  tracing::info!("conenction from {}, handling", connection_address);
+  let mut netuser =  NetUser {
+    transport: tokio_util::codec::Framed::new(connection, tokio_netstring::NetstringCodec),
+  };
+
+  
+  tokio::spawn(async move {
+    handle_connection_io( netuser, commchannel.subscribe(), commchannel.clone()).await
+  });
+  // tokio::spawn(async move {
+  //   handle_connection(connection, clientaddr,  channel ).await;
+  // });
+
+  // loop {
+
+  //   match out {
+  //     UplinkEvent::MessageReceived(x) => {
+  //       server_process(tra) // todo write driver
+  //     }
+  //   }
+    // if let Ok(message) = netuser.await_message().await {
+    //   // match server_process(transport, m)
+    // }
+    // while let Some(res) = transport.next().await {
+    //   match res {
+    //     Err(e) => tracing::error!("got error {}", e),
+    //     Ok(m) => {
+    //       match server_process(&mut transport, &m.freeze()).await {
+    //         Ok(()) => (),
+    //         Err(e) => tracing::error!("error encountered {}", e),
+    //       }
+    //     }
+    //   }
+    // };
+  // }
 }
 
 #[test]
 fn message_encode() -> anyhow::Result<()> {
-    use tokio_util::codec::Encoder;
-    use bytes::BufMut;
+  fn newStandardMessage_string_sync(s: String) -> MessageType {
+    let mut out = BytesMut::with_capacity(s.len());
+    out.extend_from_slice(s.as_bytes());
+    return MessageType::Message{payload: out}
+}
 
-    let data = "data".as_bytes();
-    let mut netstring_codec = NetstringCodec;
-    let mut output = BytesMut::new();
-    let mut message = bytes::BytesMut::new();
-    message.extend_from_slice(data);
-    netstring_codec.encode(message.freeze(), &mut output)?;
+  use tokio_util::codec::Encoder;
+  use bytes::BufMut;
 
-    let mut expect = bytes::BytesMut::new();
-    expect.put_u32_le(4);
-    expect.extend_from_slice(data);
+  let datastring = "data".to_owned();
+  let data = datastring.as_bytes();
+  let mut netstring_codec = NetstringCodec;
+  let mut output = BytesMut::new();
+  let mut message = bytes::BytesMut::new();
+  // message.extend_from_slice(data);
+  netstring_codec.encode(newStandardMessage_string_sync(datastring.clone()), &mut output)?;
 
-    assert_eq!(
-        output,
-        expect,
-    );
-    Ok(())
+  let mut expect = bytes::BytesMut::new();
+  expect.put_u32_le(data.len() as u32);
+  expect.put_u8(UPLINK_MSGT_MESSAGE);
+  expect.extend_from_slice(data);
+
+  assert_eq!(
+    output,
+    expect,
+  );
+  Ok(())
 }
--- src/tokio_netstring.rs
+++ src/tokio_netstring.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
+use bytes::Buf;
 use tokio_util::codec::Decoder;
 use tokio_util::codec::Encoder;
 
@@ -8,83 +9,146 @@ use futures::StreamExt;
 
 use bytes::{BufMut, Bytes, BytesMut};
 use std::io;
+use std::io::Read;
 use std::str;
-const MAX_WHAT: usize = 8 * 1024 * 1024;
+const UPLINK_MAXPAYLOADSIZE: usize = 8 * 1024 * 1024;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct NetstringCodec;
 
+pub static UPLINK_HEADERSIZE :usize =  5;
+pub static UPLINK_MARKERSIZE :usize =  1;
+pub static UPLINK_LENGTHSIZE :usize =  4;
+pub static UPLINK_MSGT_HEARTBEAT : u8 = 0;
+pub static UPLINK_MSGT_CONTROL   : u8 = 1;
+pub static UPLINK_MSGT_MESSAGE   : u8 = 2;
+pub static UPLINK_MSGT_TRANSFER  : u8 = 3;
+pub static UPLINK_MSGT_VOID      : u8 = 7;
+
+//-----------------------------------
+//| --- 32 --- | -- 8 -- |-- ... -- | 
+//|   length   |   type  | payload  |
+//-----------------------------------
+
+
+// not used yet todo:
+pub enum ControlType {
+    BreakOff,
+}
+  
+#[derive(Clone, Debug)]
+pub enum MessageType {        // u8 marker
+    Heartbeat,              // 0
+    Control,                // 1
+    Message {               // 2
+        payload : BytesMut,
+    },      
+    // Transfer,            // 3
+    Void,                   // 7
+  }
+
+pub async fn newStandardMessage_string(s: String) -> MessageType {
+    let mut out = BytesMut::with_capacity(s.len());
+    out.extend_from_slice(s.as_bytes());
+    return MessageType::Message{payload: out}
+}
 impl Decoder for NetstringCodec {
-    type Item = BytesMut;
+    type Item = MessageType;
     type Error = io::Error;
 
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<BytesMut>, io::Error> {
-        if src.len() < 4 {
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<MessageType>, io::Error> {
+        if src.len() < UPLINK_HEADERSIZE { // not received enough
+            return Ok(None);  // break off without mutation
+        }
+        tracing::info!("netstring::decode::src_content={:?}", src);
+        tracing::info!("netstring::decode::src_len={}", src.len());
+        // let mut header = src.copy_to_bytes(UPLINK_HEADERSIZE);
+        // copy and check the buffer length
+        let length = src.get_u32_le();
+        let marker = src.get_u8();
+        tracing::info!("netstring::decode::length={} ", length);
+        tracing::info!("netstring::decode::marker={}", marker);
+        tracing::info!("netstring::decode::src_len={}", src.len());
+
+        if length as usize > UPLINK_MAXPAYLOADSIZE {
+            tracing::info!("netstring::decode::toolarge");
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", length),
+            ));
+        } // we need to kill connection here, malformed shit in buffer
+
+        if length == 0 || marker == 0 {
+            tracing::info!("netstring::decode::got_heartbeat");
+            // heartbeat incoming, drop header, create empty message type
+            return Ok(Some(MessageType::Heartbeat));
+        } // we got mail
+
+        // not enought data yet, lets skip the rest
+        if src.len() < (length as usize) as usize { 
+            tracing::info!("netstring::decode::notenough is {} wants {}", src.len(), UPLINK_HEADERSIZE + length as usize);
             return Ok(None);
         }
 
-        let mut length_bytes = [0u8; 4];
-        length_bytes.copy_from_slice(&src[..4]);
-        let length = u32::from_le_bytes(length_bytes) as usize;
-
-        if length > MAX_WHAT {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Frame of length {} is too large.", length)
-            ));
-        }
-
-        if src.len() < 4 + length {
-            src.reserve(4 + length - src.len());
-            return Ok(None);
-        }
-
-        let out = src.split_to(4);
-        let out = src.split_to(length);
-        tracing::info!("dcode {{ size={}, data={} }}", length, str::from_utf8(&src).unwrap());
-        Ok(Some(out))
+        // shit seems okay, lets create it properly
+        // drop(src.split_to(UPLINK_HEADERSIZE)); // become ungovernable
+        tracing::info!("netstring::decode::matching?");
+        tracing::info!("netstring::decode::src_len={}", src.len());
+        // drop(src.split_to(UPLINK_HEADERSIZE));
+        match marker {
+            1 => {
+                tracing::info!("netstring::decode::got:UPLINK_MSGT_CONTROL"); 
+                return Ok(Some(MessageType::Control)) 
+            }
+            2 => {
+                tracing::info!("netstring::decode::got:UPLINK_MSGT_MESSAGE, len={}", length);  
+                return Ok(Some(MessageType::Message{ payload: src.split_to(length as usize)}))
+            },
+            // 3 => todo implement transfers
+            _ => {
+                tracing::info!("netstring::decode::got:UPLINK_MSGT_VOID");
+                return Ok(Some(MessageType::Void))
+            },
+        };
     }
 }
 
-
-impl Encoder<Bytes> for NetstringCodec {
+impl Encoder<MessageType> for NetstringCodec {
     type Error = std::io::Error;
 
-    fn encode(&mut self, item: Bytes, dst: &mut BytesMut) -> Result<(), Self::Error> {
-
-        if item.len() > MAX_WHAT {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Frame of length {} is too large.", item.len())
-            ));
+    fn encode(&mut self, item: MessageType, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        
+        match item {
+            MessageType::Heartbeat => {
+                tracing::info!("netstring::encode::heartbeat::sending");
+                dst.put_u32_le(0);
+                dst.put_u8(0);
+                tracing::info!("netstring::encode::heartbeat::dst_len={}", dst.len());
+                return Ok(())
+            },
+            MessageType::Control => {
+                // lets mask it as heartbeat for now, fuck it
+                dst.put_u32_le(0);
+                dst.put_u8(0);
+                tracing::info!("netstring::encode::Control::dst_len={}", dst.len());
+                tracing::info!("netstring::encode::control::"); // todo
+                todo!();
+            }
+            MessageType::Message{ payload: msg} => {
+                if msg.len() > UPLINK_MAXPAYLOADSIZE {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        format!("Frame of length {} is too large.", msg.len()),
+                    ));
+                }
+                dst.put_u32_le(msg.len() as u32);
+                dst.put_u8(2);
+                dst.extend_from_slice(&msg);
+                tracing::info!("netstring::encode::message::dst_len={}", dst.len());
+                tracing::info!("netstring::encode::encode {{ size={}, data={} }}", msg.len(), str::from_utf8(&msg).unwrap());
+            }
+            _ => ()
         }
-
-        let len_slice = u32::to_le_bytes(item.len() as u32);
-        dst.reserve(4 + item.len());
-
-        dst.extend_from_slice(&len_slice);
-        dst.extend_from_slice(&item);
-        tracing::info!("encode {{ size={}, data={} }}", item.len(), str::from_utf8(&item).unwrap());
-        Ok(())
-    }
-}
-
-impl Encoder<String> for NetstringCodec {
-    type Error = std::io::Error;
-
-    fn encode(&mut self, item: String, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        if item.len() > MAX_WHAT {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Frame of length {} is too large.", item.len())
-            ));
-        }
-        let len_slice = u32::to_le_bytes(item.len() as u32);
-        dst.reserve(4 + item.len());
-
-        dst.extend_from_slice(&len_slice);
-        dst.extend_from_slice(&item.as_bytes());
-        tracing::info!("encode {{ size={}, data={} }}", item.len(), item);
         Ok(())
     }
 }
